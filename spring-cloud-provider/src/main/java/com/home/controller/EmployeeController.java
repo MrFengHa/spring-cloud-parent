51ef9c756b1da47f39fd4f91ad67ffb5c792f812
MrFengHa <1109179567@qq.com>
2021-03-22 17:50:24 +0800

    设置启动logo

--- spring-cloud-provider/src/main/java/com/home/controller/EmployeeController.java
+++ spring-cloud-provider/src/main/java/com/home/controller/EmployeeController.java
@@ -1,10 +1,19 @@
 package com.home.controller;
 
+import com.home.entity.Employee;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 职工接口类
  *
  * @Author 冯根源
  * @create 2021/3/22 17:07
  */
+@RestController
 public class EmployeeController {
+    @RequestMapping("/provider/get/employee/remote")
+    public Employee getEmployeeRemote() {
+        return new Employee(555, "tom555", 555.55);
+    }
 }
