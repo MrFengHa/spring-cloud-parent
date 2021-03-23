3b5ab2d72ac6ff345463d5a6baea937ec411ab27
MrFengHa <1109179567@qq.com>
2021-03-23 10:07:05 +0800

    将消费者注册到Eureka 通过服务名称访问即可

--- spring-cloud-consumer/src/main/java/com/home/controller/HumanResourceController.java
+++ spring-cloud-consumer/src/main/java/com/home/controller/HumanResourceController.java
@@ -15,11 +15,13 @@ public class HumanResourceController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @RequestMapping("/consumer/get/employee/remote")
+    @RequestMapping("/consumer/ribbon/get/employee")
     public Employee getEmployeeRemote() {
         //1.声明远程微服务主机地址加端口号
-        String host = "http://localhost:1000";
+        //String host = "http://localhost:1000";
 
+        //将运城微服务调用地址从“IP地址+端口号”改成“微服务名称”
+        String host ="http://rootFeng-provider";
         //2.声明具体要调用的URl功能
         String url = "/provider/get/employee/remote";
 
